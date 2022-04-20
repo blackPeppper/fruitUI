fa63fe7f5adb1d2e5a609d34c11bedf93203147b
unknown <medohossam.g@gmail.com>
2022-04-20 11:48:57 +0200

    change a npx command and remove a ex-time library

--- cli/index.js
+++ cli/index.js
@@ -1,36 +1,15 @@
 #!user/bin/env node
-
-// import fs from "fs";
-// import { createSpinner } from "nanospinner";
-// import chalk from "chalk";
-// import sassCompiler from "./sassCompiler.js";
-// import fruit from './fruit.js';
-// const fruit = require('fruit.js');
-const sassCompiler = require('./sassCompiler.js');
-const fruit = require('./fruit.js');
+const process = require("process")
 const fs = require('fs');
-const {createSpinner} = require('nanospinner');
-var spinner = createSpinner("watching").start();
+if (process.argv[2] == "init") {
+    fs.writeFile('../../../config.json', "test", err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    })
+    console.log("init file craeted")
+    process.exit(0)
+}
 
-
-let varsassPath = fruit.srcPath;
-let varcssPath = fruit.cssPath;
-// let varsassPath = "./main.scss";
-// let varcssPath = "./i.css";
-
-fs.watch(varsassPath,{ interval: 100 },(curr, prev) => {
-    // start the timer
-    try {
-      let timeone = performance.now();
-      sassCompiler(varcssPath, varsassPath);
-      // end the timer
-      let timetow = performance.now();
-      var time = Math.floor(timetow - timeone);
-      console.clear();
-      spinner.success({ text: `done in ${time} ms` });
-    } catch (e) {
-      console.clear();
-      spinner.error({ text: ` ${e.stack}` });
-    }
-  }
-);
