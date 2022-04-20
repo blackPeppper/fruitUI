3b6e899ac91a9e2fa68af2a1fdf5f7b26e24220a
unknown <medohossam.g@gmail.com>
2022-04-20 15:59:33 +0200

    try to fix npx again

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# fruitUI
-readne tset
+# npmtest
--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-#!user/bin/env node
-console.log("hellow");
+#!/usr/bin/env node
+const process = require("process")
+const fs = require('fs');
+if (process.argv[2] == "init") {
+    fs.writeFile('../../../config.json', "test", err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    })
+    console.log("init file craeted")
+    process.exit(0)
+}
+console.log("hello its working")
